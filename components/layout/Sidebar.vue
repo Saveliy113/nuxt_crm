45a16b859e1db34a35378dc00690163ecb87a653
Saveliy <sava2698@gmail.com>
2024-01-19 23:32:24 +0600

    Uploaded

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <aside class="px-5 py-8 bg-red h-full w-full relative">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg src="/logo.svg" alt="logo" width="100px" class="mx-auto" />
+    </NuxtLink>
+  </aside>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="grid" style="min-height: 100vh">
+    <LayoutSidebar />
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+</style>
